bc6e5558b50edc3938951305284d69fcc386c63f
isadora caliari thomazella <107407815+rafaelaisadora@users.noreply.github.com>
2024-05-13 12:28:52 -0300

    isa

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const dataAtual = new Date ();
 let dataObjetivo = prompt('' Data final ANO-MES-DIA
-Exemplo: 2024-04-10'')
+Exemplo: 2024-04-10''); 
+
+
+dataObjetivo = new Date (dataObjetivo + "T23:59:59");
+let diasQueFaltam = math.floor ((dataObjetivo - dataAtual)/ 86400000);
+
+
+document.querySelector("#dias_restantes").textContent = diasQueFaltam;
+
+
